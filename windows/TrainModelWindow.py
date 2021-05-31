86323864a157ceb49389bbd2befb70486069abf3
bcilab601 <bcilab601>
2021-05-31 17:36:33 +0800

    test

--- windows/TrainModelWindow.py
+++ windows/TrainModelWindow.py
@@ -48,38 +48,41 @@ class TrainModelWindow(wx.Dialog):
     def on_close(self, event):
         self.Close()  # 关闭窗体
 
-    def on_train_model(self, event):
-        data = MIdataset(self.train_path_ctrl.GetPath())
-        data.bandpass_filter(1, 100)  # band pass
-        data.set_reference()  # CAR
-
+    def get_UA_RP(self, baseline_eo):
+        # get peak alpha frequency(paf) from eye_closed baseline
         ec_baseline_pre = MIdataset(self.save_model_path + r'/ec_baseline_pre.npz')
         ec_baseline_pre.bandpass_filter(1, 100)  # band pass
         paf = ec_baseline_pre.get_IAF()
+        base_rela_UA_power = cal_power_feature(baseline_eo, pick_rest_ch, freq_min=paf, freq_max=paf + 2, rp=True)
+        return base_rela_UA_power
 
-        eo_baseline_pre = dict(np.load(self.save_model_path + r'/eo_baseline_pre.npz', allow_pickle=True))
-        baseline_pre_signal = eo_baseline_pre['signal']
-        base_UA_power = cal_power_feature(baseline_pre_signal, pick_rest_ch, freq_min=paf, freq_max=paf + 2, rp=True)
-
-        # data.removeEOGbyICA()  # ICA
+    def get_individual_LR(self, baseline_eo):
+        data = MIdataset(self.train_path_ctrl.GetPath())
+        data.bandpass_filter(1, 100)  # band pass
+        data.set_reference()  # CAR
         data.bandpass_filter(8, 30)
         select_ch = pick_motor_ch  # M1附近区域
         data_array, label = data.get_epoch_data(select_label=['Left', 'Right'], select_ch=select_ch)
         selected_ch_names = iterative_CSP_LR(data_array, select_ch)  # csp select
         left_ch = selected_ch_names[:, 0]
         right_ch = selected_ch_names[:, 1]
-        eo_baseline_pre = dict(np.load(self.save_model_path + r'/eo_baseline_pre.npz', allow_pickle=True))
+        base_leftch_power = cal_power_feature(baseline_eo, left_ch, freq_min=8, freq_max=30)
+        base_rightch_power = cal_power_feature(baseline_eo, right_ch, freq_min=8, freq_max=30)
+        return left_ch, right_ch, base_leftch_power, base_rightch_power
 
-        baseline_pre_signal = eo_baseline_pre['signal']
-        base_UA_power = cal_power_feature(baseline_pre_signal, pick_rest_ch, freq_min=paf, freq_max=paf+2, rp=True)
-        base_leftch_power = cal_power_feature(baseline_pre_signal, left_ch, freq_min=8, freq_max=30)
-        base_rightch_power = cal_power_feature(baseline_pre_signal, right_ch, freq_min=8, freq_max=30)
-        np.savez(self.save_model_path + r'/model', left_ch=left_ch, right_ch=right_ch, base_alpha_power=base_UA_power,
-                 base_leftch_power=base_leftch_power, base_rightch_power=base_rightch_power)
+    def on_train_model(self, event):
+        eo_baseline_pre = dict(np.load(self.save_model_path + r'/eo_baseline_pre.npz', allow_pickle=True))
+        baseline_eo = eo_baseline_pre['signal']
+        UA_RP = self.get_UA_RP(baseline_eo)
+        # left_ch, right_ch, base_leftch_power, base_rightch_power = self.get_individual_LR(baseline_eo)
+        # np.savez(self.save_model_path + r'/model', left_ch=left_ch, right_ch=right_ch, base_alpha_rela_power=UA_RP,
+        #          base_leftch_power=base_leftch_power, base_rightch_power=base_rightch_power)
+        np.savez(self.save_model_path + r'/model',  base_alpha_rela_power=UA_RP)
         # df.to_csv(self.save_model_path, header=False, index=False)  # save selected channels
         self.statusLabel.SetLabel('模型训练完成。')
 
 
-
-
-
+if __name__ == '__main__':
+    model = dict(np.load(r'C:\StrokeEEGProj\codes\MIBCIProj_NF\data_set\S1\S1_20210528\model.npz'))
+    base_alpha_rela_power = model['base_alpha_rela_power']
+    print('1')
